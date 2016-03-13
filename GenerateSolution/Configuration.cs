6826b1e2b1e9a96e203be9deb1fa1ab8a07ea3b2
Björn Hellander <bjorn_hellander@yahoo.se>
2016-03-13 17:34:36 +0100

    Updated Configuration class so the generated solution is large

--- GenerateSolution/Configuration.cs
+++ GenerateSolution/Configuration.cs
@@ -4,12 +4,12 @@
     {
         public static string SolutionName { get; set; } = "TestSolution";
 
-        public static int NumberOfProjects { get; set; } = 1;
+        public static int NumberOfProjects { get; set; } = 200;
 
-        public static int NumberOfClasses { get; set; } = 1;
+        public static int NumberOfClasses { get; set; } = 30;
 
-        public static int NumberOfMethods { get; set; } = 1;
+        public static int NumberOfMethods { get; set; } = 20;
 
-        public static int NumberOfStatements { get; set; } = 1;
+        public static int NumberOfStatements { get; set; } = 30;
     }
 }
